d89198d973d93d328fcb50193f2e067bfb414af2
Prabal Manchanda <manchandaprabal878@gmial.com>
2023-04-01 02:02:28 -0400

    Added log in and sign in button and a home page too

--- src/fitness/User.java
+++ src/fitness/User.java
@@ -1,37 +1,47 @@
 package fitness;
 
+import java.awt.Image;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.geometry.HPos;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 
 public class User extends Application {
     
     TextField fullname=new TextField();
-    TextField email=new TextField();
-    TextField conformemail=new TextField();
-    TextField phonenumber=new TextField();
-    TextField password=new TextField();
-    Button submit=new Button("Submit");
     
-
+    TextField email=new TextField();
+    
+    TextField conformemail=new TextField();
+    
+    TextField phonenumber=new TextField();
+    
+    TextField password=new TextField();
+    
+    Button submit=new Button("Submit");
 
     @Override
     public void start(Stage window) {
-        
+        BorderPane layout=new BorderPane();
         window.setTitle("Welcome");
+        //ImageView image=new ImageView("main.jpg");
         
-        HBox main=new HBox();
+        HBox main=new HBox(20);
+        main.setPadding(new Insets(20,20,50,50));
         
         Button login=new Button("log in");
         Button signup=new Button("Sign up");
@@ -42,69 +52,107 @@ public class User extends Application {
         
         
         main.getChildren().addAll(login,signup);
-        main.setAlignment(Pos.CENTER);
-        BorderPane layout=new BorderPane();
-        layout.setCenter(main);
+        
+        main.setAlignment(Pos.TOP_RIGHT);
+        
+        Line line=new Line(0,100,500,100);
+        layout.setTop(main);
+        layout.getChildren().add(line);
         Scene scene=new Scene(layout,500,500);
+        
         window.setScene(scene);;
         window.show();
             
     }
+    
     public static void main(String[] args) {
+        
         launch(args);
+        
     }
     
     public void signup() {
+        
         Stage signStage=new Stage();
         GridPane pane=new GridPane();
         
         //Fullname label
         pane.add(new Label("Full Name"), 0, 0);
         pane.add(fullname, 1, 0);
+        
         fullname.setAlignment(Pos.BOTTOM_RIGHT);
 
         //Email label
         pane.add(new Label("Email"), 0, 1);
         pane.add(email, 1, 1);
+        
         email.setAlignment(Pos.BOTTOM_RIGHT);
         
         //Conformemail label
         pane.add(new Label("Conform Email"), 0, 2);
         pane.add(conformemail, 1, 2);
+        
         conformemail.setAlignment(Pos.BOTTOM_RIGHT);
         
         //Phonenumber label
         pane.add(new Label("Phone Number"), 0, 3);
         pane.add(phonenumber, 1, 3);
+        
         phonenumber.setAlignment(Pos.BOTTOM_RIGHT);
         
         //password label
         pane.add(new Label("Set Password"), 0, 4);
         pane.add(password, 1, 4);
+        
         password.setAlignment(Pos.BOTTOM_RIGHT);
         
-        //Submit buttom
-        pane.add(submit, 1, 5);
-        GridPane.setHalignment(submit, HPos.RIGHT);
+        Button sign=new Button("Sign in");
+        pane.add(sign, 1, 5);
+        GridPane.setHalignment(sign, HPos.RIGHT);
+        
+        Label mess=new Label("");
+        pane.add(mess, 0, 6);
+        
+        
+        
+        if(fullname.getText()==null||email.getText()==null||conformemail.getText()==null||phonenumber.getText()==null||password.getText()==null) {
+            mess.setText("Please fill all the details");
+            mess.setTextFill(Color.RED);
+        }
+        else {
+            sign.setOnAction(e->plan());
+        }
         
         
         pane.setAlignment(Pos.CENTER);
         Scene scene=new Scene(pane,500,500);
+        
         signStage.setScene(scene);
         signStage.show();
+        
     }
+    
     public void login() {
+        
         Stage logStage =new Stage();
+        
         GridPane layout =new GridPane();
+        
         TextField emailtextField=new TextField();
         layout.add(new Label("Email"), 0, 2);
+        
         layout.add(emailtextField, 1, 2);
         emailtextField.setAlignment(Pos.BOTTOM_RIGHT);
         
         TextField passwordtextField=new TextField();
         layout.add(new Label("Password"), 0, 1);
+        
         layout.add(passwordtextField, 1, 1);
         passwordtextField.setAlignment(Pos.BOTTOM_RIGHT);
+        
+        Button log=new Button("Log in");
+        layout.add(log, 1, 5);
+        GridPane.setHalignment(log, HPos.RIGHT);
         
         layout.setAlignment(Pos.CENTER);
         
@@ -112,6 +160,21 @@ public class User extends Application {
         
         logStage.setScene(scene);
         logStage.show();
+        
+    }
+    public void plan() {
+        Stage plans=new Stage();
+        Label heading=new Label("Please choose your plans");
+        BorderPane pane=new BorderPane();
+        pane.setTop(heading);
+        
+        
+        Scene scene=new Scene(pane,500,500);
+        plans.setScene(scene);
+        plans.show();
+        
+        
+        
         
     }
     
