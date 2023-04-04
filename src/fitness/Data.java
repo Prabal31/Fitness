9874ec132d966ac774167a52fb56a6f51a3fe6da
Prabal Manchanda <manchandaprabal878@gmial.com>
2023-04-04 03:06:07 -0400

    added button to goal page

--- src/fitness/Data.java
+++ src/fitness/Data.java
@@ -2,14 +2,21 @@ package fitness;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Scanner;
 import javafx.geometry.Insets;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
 import javafx.geometry.VPos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.control.TextField;
@@ -75,24 +82,25 @@ public class Data {
 
         }
     }
-    public void print(Image background,Image logo,ImageView Logo,String key) {
-        Stage home = new Stage();
-        home.setTitle("Data");
+    
+    
+    public void heightweight(TextField email) {
         
-        GridPane grid=new GridPane();
-        grid.setHgap(10);
-        grid.setVgap(10);
-        grid.setPadding(new Insets(20));
+        Stage stage=new Stage();
+        GridPane gridPane = new GridPane();
         
-        grid.setAlignment(Pos.TOP_CENTER);
+        gridPane.setAlignment(Pos.TOP_CENTER);
         
-        home.getIcons().add(logo);
+        gridPane.setVgap(15);
+        stage.setTitle("Welcome");
+
+        stage.getIcons().add(logo);
 
         // Set the properties of the image view
         BackgroundImage view = new BackgroundImage(background,
         BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
-        new BackgroundSize(home.getWidth(), home.getHeight(), false, false, false, true));
-        grid.setBackground(new Background(view));
+        new BackgroundSize(stage.getWidth(), stage.getHeight(), false, false, false, true));
+        gridPane.setBackground(new Background(view));
         
         Logo.setFitWidth(100);
         Logo.setFitHeight(100);
@@ -103,94 +111,159 @@ public class Data {
         Logobox.getChildren().add(Logo);
         
         Logobox.setAlignment(Pos.CENTER);
-        grid.add(Logobox, 0, 0,2,1);
+        gridPane.add(Logobox, 0, 0,2,1);
         
-        GridPane.setValignment(Logobox, VPos.TOP);
+        Logobox.setAlignment(Pos.CENTER);
+        // Create labels for height and weight
         
-        Label heading = new Label("Details");
-        heading.setAlignment(Pos.CENTER);
-        heading.setStyle("-fx-font-weight: bold; -fx-font-size: 20px;");
-        // Add fields to HBox
+        Label heightLabel = new Label("Height:");
+        Label weightLabel = new Label("Weight:");
+        
+        // Create text fields for entering height and weight
+        TextField heightTextField = new TextField();
+        TextField weightTextField = new TextField();
+        
+        // Create a GridPane to contain the labels and text fields
+        
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        
+        gridPane.setPadding(new Insets(10));
+        
+        // Add the labels and text fields to the GridPane
+        gridPane.add(heightLabel, 0, 1);
+        
+        gridPane.add(heightTextField, 1, 1);
+        
+        gridPane.add(weightLabel, 0, 2);
+        gridPane.add(weightTextField, 1, 2);
+        
+       
+        
+        // Create a button to calculate the BMI
+        Button calculateButton = new Button("Calculate your BMI");
+        
+        calculateButton.setStyle("-fx-background-color:#4CAF50; -fx-text-fill:white;");
+        gridPane.add(calculateButton, 0, 3);
 
-        try {
-            BufferedReader reader = new BufferedReader(new FileReader("user_data.txt"));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                String[] details = line.split(",");
-                String name = details[0];
-                String email = details[1];
-                String phone = details[2];
+        calculateButton.setOnAction(event -> {
+            
+            if(heightTextField.getText().isEmpty() || weightTextField.getText().isEmpty()) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                
+                alert.setTitle("Error");
+                
+                alert.setHeaderText("Please fill in the details");
+                alert.showAndWait();
+            }
+            else {
+                double height = Double.parseDouble(heightTextField.getText());
+                double weight = Double.parseDouble(weightTextField.getText());
+                
+                double bmi = weight / (height * height);
 
-                if (name.contains(key) || email.contains(key) || phone.contains(key)) {
-                    // display or do something with the person's details
-                    // Add labels and text fields to HBox
-                    HBox namebox=new HBox();
+                // Create an Alert box to display the BMI
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setTitle("BMI Calculator");
+                
+                alert.setHeaderText("Your BMI is:");
+                alert.setContentText(String.format("%.2f", bmi));
+                
+                alert.showAndWait();
+                
+                try {
+                    FileInputStream file = new FileInputStream("user_data.txt");
+                    Scanner scan=new Scanner(file);
                     
-                    Label nameLabel = new Label("Name:");
-                    nameLabel.setFont(Font.font(35));
-                    
-                    nameLabel.setTextFill(Color.AQUA);
-                    nameLabel.setAlignment(Pos.CENTER_RIGHT);
-                    
-                    Label nameField = new Label(name);
-                    nameField.setFont(Font.font(35));
-                    nameField.setTextFill(Color.POWDERBLUE);
-                    namebox.getChildren().addAll(nameLabel,nameField);
-                    
-                    grid.add(namebox, 0, 1);
-                    
-                    
-                    
-                    HBox emailbox=new HBox();
-                    
-                    Label emailLabel = new Label("Email:");
-                    emailLabel.setFont(Font.font(35));
-                    
-                    emailLabel.setTextFill(Color.AQUA);
-                    emailLabel.setAlignment(Pos.CENTER_RIGHT);
-                    
-                    Label emailField = new Label(email);
-                    emailField.setFont(Font.font(35));
-                    emailField.setTextFill(Color.POWDERBLUE);
-                    
-                    emailbox.getChildren().addAll(emailLabel,emailField);
-                    
-                    grid.add(emailbox, 0, 2);
-                    
-                    
-                    
-                    HBox phonebox=new HBox();
-                    
-                    Label phoneLabel = new Label("Phone:");
-                    phoneLabel.setFont(Font.font(35));
-                    
-                    phoneLabel.setTextFill(Color.AQUA);
-                    phoneLabel.setAlignment(Pos.CENTER_RIGHT);
-                    
-                    Label phoneField = new Label(phone);
-                    phoneField.setFont(Font.font(35));
-                    phoneField.setTextFill(Color.POWDERBLUE);
-                    
-                    phonebox.getChildren().addAll(phoneLabel,phoneField);
-                    
-                    grid.add(phonebox, 0, 3);
+                    ArrayList<String> updatedLines = new ArrayList<>();
 
+                    
+                    while (scan.hasNextLine()) {
+                        String line = scan.nextLine();
+                        
+                        String[] parts = line.split(",");
+                        String username = parts[1];
+                        
+                        if (username.equals(email.getText())) {
+                            
+                            // Add height and weight data for this user
+                            String setheight = heightTextField.getText();
+                            
+                            String setweightt = weightTextField.getText();
+                            double setbmi=bmi;
+                            line = line + "," + setheight + "," + setweightt+","+setbmi;
+                            
+                        }
+                        
+                        // Write the updated line back to the fil
+                        updatedLines.add(line);
+                    }
+
+
+                        Files.write(Paths.get("user_data.txt"), updatedLines);
+
+                } 
+                catch (IOException e) {
+                    
+                    // Handle the exception here
+                    e.printStackTrace();
+                    
                 }
             }
-            reader.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        Scene scene = new Scene(grid, 400, 550);
-        home.setScene(scene);
-        home.show();
+            
+        });
+        
+        HBox nextbuttonbox =new HBox();
+        Button nextbutton=new Button("Set you goal");
+        
+        nextbuttonbox.setAlignment(Pos.CENTER);
+        nextbuttonbox.getChildren().add(nextbutton);
+        
+        nextbutton.setStyle("-fx-background-color:#4CAF50; -fx-text-fill:white;");
+        gridPane.add(nextbuttonbox, 0, 4);
+        
+
+        nextbutton.setOnAction(e-> {
+            
+            if(heightTextField.getText().isEmpty() || weightTextField.getText().isEmpty()) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                
+                alert.setTitle("Error");
+                alert.setHeaderText("Please fill in the details");
+                
+                alert.showAndWait();
+            }
+            
+            else {
+
+            goal(email);
+            stage.close();
+            
+            }
+        });
+        
+
+        
+         // Create a scene with the GridPane as the root node
+        Scene scene = new Scene(gridPane, 400, 550);
+        // Set the stage title and scene, and show the stage
+        
+        stage.setTitle("Height and Weight Input");
+        stage.setScene(scene);
+        
+        stage.show();
     }
     
     
-    public void goal() {
+    
+    
+    
+    public void goal(TextField email) {
         Stage goal =new Stage();
+        
         GridPane pane = new GridPane();
         pane.setHgap(10);
+        
         pane.setVgap(10);
         pane.setPadding(new Insets(20));
         
@@ -250,24 +323,197 @@ public class Data {
         // Create a slider with start, mid, and end values
         Slider slider = new Slider();
         slider.setOrientation(Orientation.HORIZONTAL);
-        slider.setMin(0);
-        slider.setMax(2);
-        slider.setValue(1);
+        slider.setMin(1);
+        slider.setMax(3);
+        
+        slider.setValue(2);
 
         // Set the tick marks and labels for the slider
         slider.setMajorTickUnit(1);
         slider.setMinorTickCount(0);
+        
         slider.setShowTickLabels(true);
         slider.setShowTickMarks(true);
+        
         slider.setSnapToTicks(true);
 
         // Create a layout and add the slider and labels
         
         pane.add(slider, 0, 3,2,1);
+        
+        try {
+            FileInputStream file = new FileInputStream("user_data.txt");
+            Scanner scan=new Scanner(file);
+            ArrayList<String> updatedLines = new ArrayList<>();
 
+
+            while (scan.hasNextLine()) {
+                
+                String line = scan.nextLine();
+                String[] parts = line.split(",");
+                
+                String username = parts[1];
+                
+                if (username.equals(email.getText())) {
+                    
+                    // Add height and weight data for this user
+                    double difficultlevel=slider.getValue();
+                    line = line + "," +difficultlevel;
+                }
+                
+                // Write the updated line back to the fil
+                updatedLines.add(line);
+            }
+            
+            Files.write(Paths.get("user_data.txt"), updatedLines);
+
+        } 
+        
+        catch (IOException e) {
+            
+            // Handle the exception here
+            e.printStackTrace();
+        }
+        HBox mainbox=new HBox();
+        Button main=new Button("Submit");
+        main.setStyle("-fx-background-color:#4CAF50; -fx-text-fill:white;");
+        
+        mainbox.getChildren().add(main);
+        mainbox.setAlignment(Pos.CENTER);
+        
+        pane.add(mainbox, 0, 4,2,1);
+
+        String key=email.getText();
+        main.setOnAction(e-> {
+            
+            print(key);
+            
+        });
+        
         // Set the scene and show the stage
         Scene scene = new Scene(pane, 400, 550);
         goal.setScene(scene);
         goal.show();
+    }
+    
+    
+    
+    public void print(String key) {
+        Stage home = new Stage();
+        home.setTitle("Data");
+        
+        GridPane grid=new GridPane();
+        grid.setHgap(10);
+        grid.setVgap(10);
+        grid.setPadding(new Insets(20));
+        
+        grid.setAlignment(Pos.TOP_CENTER);
+        
+        home.getIcons().add(logo);
+
+        // Set the properties of the image view
+        BackgroundImage view = new BackgroundImage(background,
+        BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
+        new BackgroundSize(home.getWidth(), home.getHeight(), false, false, false, true));
+        grid.setBackground(new Background(view));
+        
+        Logo.setFitWidth(100);
+        Logo.setFitHeight(100);
+        
+        
+        
+        HBox Logobox=new HBox();
+        Logobox.getChildren().add(Logo);
+        
+        Logobox.setAlignment(Pos.CENTER);
+        grid.add(Logobox, 0, 0,2,1);
+        
+        GridPane.setValignment(Logobox, VPos.TOP);
+        
+        Label heading = new Label("Details");
+        heading.setAlignment(Pos.CENTER);
+        heading.setStyle("-fx-font-weight: bold; -fx-font-size: 20px;");
+        // Add fields to HBox
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("user_data.txt"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] details = line.split(",");
+                String name = details[0];
+                String email = details[1];
+                String phone = details[2];
+
+
+                if (name.contains(key) || email.contains(key) || phone.contains(key)) {
+                    // display or do something with the person's details
+                    // Add labels and text fields to HBox
+                    HBox namebox=new HBox();
+                    
+                    Label nameLabel = new Label("Name:");
+                    nameLabel.setFont(Font.font(25));
+                    
+                    nameLabel.setTextFill(Color.AQUA);
+                    nameLabel.setAlignment(Pos.CENTER_RIGHT);
+                    
+                    Label nameField = new Label(name);
+                    nameField.setFont(Font.font(20));
+                    nameField.setTextFill(Color.POWDERBLUE);
+                    namebox.getChildren().addAll(nameLabel,nameField);
+                    
+                    grid.add(namebox, 0, 1);
+                    
+                    
+                    
+                    HBox emailbox=new HBox();
+                    
+                    Label emailLabel = new Label("Email:");
+                    emailLabel.setFont(Font.font(25));
+                    
+                    emailLabel.setTextFill(Color.AQUA);
+                    emailLabel.setAlignment(Pos.CENTER_RIGHT);
+                    
+                    Label emailField = new Label(email);
+                    emailField.setFont(Font.font(20));
+                    emailField.setTextFill(Color.POWDERBLUE);
+                    
+                    emailbox.getChildren().addAll(emailLabel,emailField);
+                    
+                    grid.add(emailbox, 0, 2);
+                    
+                    
+                    
+                    HBox phonebox=new HBox();
+                    
+                    Label phoneLabel = new Label("Phone:");
+                    phoneLabel.setFont(Font.font(25));
+                    
+                    phoneLabel.setTextFill(Color.AQUA);
+                    phoneLabel.setAlignment(Pos.CENTER_RIGHT);
+                    
+                    Label phoneField = new Label(phone);
+                    phoneField.setFont(Font.font(20));
+                    phoneField.setTextFill(Color.POWDERBLUE);
+                    
+                    phonebox.getChildren().addAll(phoneLabel,phoneField);
+                    
+                    grid.add(phonebox, 0, 3);
+                    
+                    
+                    
+                }
+            }
+            
+            reader.close();
+        } 
+        
+        catch (IOException e) {
+            
+            e.printStackTrace();
+        }
+        Scene scene = new Scene(grid, 400, 550);
+        home.setScene(scene);
+        
+        home.show();
     }
 }
